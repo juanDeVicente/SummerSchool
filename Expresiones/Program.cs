42d85a423a92bb7c7a34d3e1c2e64a4c0f7f909c
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2018-06-23 20:25:59 +0200

    Código relacionado con el apartado de expresiones. Evalúa la expresión 5 +2 de tres maneras distintas.

---
+++ Expresiones/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Expresiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + 2);
+            Console.WriteLine("5" + "2");
+            Console.WriteLine("5" + 2);
+            Console.ReadKey();
+        }
+    }
+}
