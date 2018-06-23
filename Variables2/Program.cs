1d55de6ff2b3b3c4a019e48b1c7a3abe7e7aad70
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2018-06-23 20:30:03 +0200

    Este código muestra una sencilla forma de que el usuario introduzca su nombre y el programa le salude y se despida

---
+++ Variables2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = Console.ReadLine(); //En la variable almacenamos el valor del texto que el usuario introduce por la pantalla
+
+            Console.WriteLine("¡Hola, " + nombre + "!"); //Le pedimos que imprima por pantalla "¡Hola, ", le concatenamos lo que esté almacenado en la variable nombre y le concatenamos "!"
+            Console.WriteLine("¡Adios, " + nombre + "!"); //Le pedimos que imprima por pantalla "¡Adios, ", le concatenamos lo que esté almacenado en la variable nombre y le concatenamos "!"
+        }
+    }
+}
