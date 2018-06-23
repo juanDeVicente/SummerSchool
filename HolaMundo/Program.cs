e424e60bba019581ff519c8ea68324a6f7e3d646
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2018-06-23 20:27:49 +0200

    El clásico hola mundo en C#.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)// Lo que esté dentro de esta frase entre llaves ({, }) es lo que define el punto de entrada de la aplicación.
+        {
+            Console.WriteLine("Hello World!"); //Esta línea sirve para que se imprima por el cmd la frase: Hello World!
+            Console.ReadKey(); //Esta línea sirve para que el programa no continue hasta que el usuario pulse una tecla
+        }
+    }
+}
