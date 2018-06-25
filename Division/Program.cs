2be66b842de2ea3485a0c88be30121abd80ddc5e
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2018-06-25 22:38:12 +0200

    Una posible solución al problema de la división.

---
+++ Division/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividendo;
+            int divisor;
+            int cociente;
+            int resto;
+
+            Console.Write("Dividendo: ");
+            dividendo = int.Parse(Console.ReadLine());
+            Console.Write("Divisor: ");
+            divisor = int.Parse(Console.ReadLine());
+            cociente = dividendo / divisor;
+            resto = dividendo % divisor;
+
+            if (resto == 0)
+                Console.WriteLine("La división es exacta");
+            else
+                Console.WriteLine("La división no es exacta");
+            Console.WriteLine("Cociente: " + cociente);
+            Console.WriteLine("Resto: " + resto);
+            Console.ReadKey();
+        }
+    }
+}
