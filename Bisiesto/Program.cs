8060388f43b82a00978684c31104cbe02114d2ad
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2018-06-25 22:37:56 +0200

    Una posible solución al problema de los años bisiestos.

---
+++ Bisiesto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un año");
+            int anio = int.Parse(Console.ReadLine());
+            if (anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0)
+                Console.WriteLine("Es bisiesto");
+            else
+                Console.WriteLine("No es bisiesto");
+            Console.ReadKey();
+        }
+    }
+}
