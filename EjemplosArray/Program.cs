21596bf60568360713a8a32c37a6584897117cbe
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2018-06-27 09:49:51 +0200

    Un ejemplo de uso de array

---
+++ EjemplosArray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EjemplosArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array;
+            Console.Write("Introduce un número:");
+            int n = 0;
+            while(!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.Clear();
+                Console.Write("Introduce un número: ");
+            }
+            array = FillRandom(n);
+            Console.WriteLine("Tu array ha quedado de la siguiente manera:");
+            Console.WriteLine(Join(array));
+            Console.WriteLine("Su suma acumulada es: " + CumSum(array));
+            Console.WriteLine("Su media es: " + Average(array));
+            Console.ReadKey();
+        }
+        static int[] FillRandom(int n)
+        {
+            int[] array = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next(10);
+            return array;
+        }
+        static int CumSum(int[] array)
+        {
+            int cumSum = 0;
+            foreach (int element in array)
+                cumSum += element;
+            return cumSum;
+        }
+        static double Average(int[] array)
+        {
+            return CumSum(array) * 1.0 / array.Length;
+        }
+        static string Join(int[] array)
+        {
+            if (array.Length == 1)
+                return "[" + array[0] + "]";
+            string join = "[" + array[0] + ", ";
+            for (int i = 1; i < array.Length - 1; i++)
+                join += array[i] + ", ";
+            if(array.Length > 1)
+                join += array[array.Length - 1] + "]";
+
+            return join;
+        }
+    }
+}
