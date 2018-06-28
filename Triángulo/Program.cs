08f11a33a85198a23d2ec823d9debfe61c165552
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2018-06-28 09:40:09 +0200

    Una posible solución al problema del triángulo.

---
+++ Triángulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Triángulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura;
+            Console.Write("Introduce un número: ");
+            while(!int.TryParse(Console.ReadLine(), out altura) && altura > 1)
+            {
+                Console.Clear();
+                Console.Write("Introduce un número: ");
+            }
+            int numeroEspacios = altura - 1;
+            int asteriscos = 1;
+            for(int i = 0; i < altura; i++)
+            {
+                Console.Write(new string(' ', numeroEspacios));
+                Console.WriteLine(new string('*', asteriscos));
+                numeroEspacios--;
+                asteriscos += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
